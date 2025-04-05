933e5fc475add4d473a4bbdb3f8bd79f77034c97
Bruno Profeta <brunoprofetacavalcante@gmail.com>
2025-04-05 10:21:53 -0300

    feature: Criando Projeto

---
+++ src/main/java/com/fiap/hackathon_fiap_sus/HackathonFiapSusApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.hackathon_fiap_sus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonFiapSusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonFiapSusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/hackathon_fiap_sus/HackathonFiapSusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.hackathon_fiap_sus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonFiapSusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
